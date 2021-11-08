a1875df4e4b303a9040117ac0ee48ea3544a0e2f
Kendall Pinto <84712013+KendallDoesCoding@users.noreply.github.com>
2021-11-08 22:40:10 +0530

    Update game.js

--- game.js
+++ game.js
@@ -91,13 +91,6 @@ let questions = [
         choice4: '5 million',
         answer: 3
     },
-        question: 'What is the population of Switzerland?',
-        choice1: '3.5 million',
-        choice2: '10 million',
-        choice3: '8.6 million',
-        choice4: '5 million',
-        answer: 3
-    },
 
 ]
 
