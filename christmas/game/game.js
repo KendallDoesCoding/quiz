d93c4439670e81fe0e6104f46a7ae132cac8d92f
Kendall Pinto <github@kendalldoescoding.gq>
2021-11-30 11:27:09 +0530

    removed .html from end path

--- christmas/game/game.js
+++ christmas/game/game.js
@@ -148,7 +148,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/christmas/end.html')
+        return window.location.assign('/christmas/end')
     }
 
     questionCounter++
