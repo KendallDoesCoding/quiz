4b7aa087b2c99eda830f0268c0fa9efee0c69fd6
Kendall Pinto <github@kendalldoescoding.gq>
2021-11-30 11:26:38 +0530

    changed path to /fruit/end, removed .htmlo

--- fruit/game/game.js
+++ fruit/game/game.js
@@ -107,7 +107,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html')
+        return window.location.assign('/fruit/end')
     }
 
     questionCounter++
