bc18cea3a52e762e401c4e9bf3bc1e1f91601dd9
Kendall Pinto <kendalldoescoding@gmail.com>
2022-01-05 12:31:19 +0530

    added 3 tech related question
    rest of the questions are still christmas related taken from the christmas topic quiz

--- tech/game/game.js
+++ tech/game/game.js
@@ -12,28 +12,28 @@ let availableQuestions = []
 
 let questions = [
     {
-        question: 'Which popular Christmas beverage is also called “milk punch?',
-        choice1: 'Eggnog',
-        choice2: 'Hot Toddy',
-        choice3: 'Cranberry-Apple Cider Punch',
-        choice4: 'Wassail',
+        question: 'About how many computer languages are in use?',
+        choice1: '10,000',
+        choice2: '50',
+        choice3: '2,000',
+        choice4: '5,000',
+        answer: 3
+    },
+    {
+        question: 'What device, released in 1993, gave rise to the term personal digital assistant?',
+        choice1: 'Newton MessagePad',
+        choice2: 'Clippy by Microsoft',
+        choice3: 'Palm Pilot',
+        choice4: 'Cortana',
         answer: 1
     },
     {
-        question: 'In Home Alone, where are the McCallisters going on vacation when they leave Kevin behind?',
-        choice1: 'United States of America',
-        choice2: 'Japan',
-        choice3: 'Dubai',
-        choice4: 'Paris',
+        question: 'Who coined the term "artificial intelligence"?',
+        choice1: 'Herbert A. Simon',
+        choice2: 'Charles BachmanEngland',
+        choice3: 'Donald Ervin Knuth',
+        choice4: 'John McCarthy',
         answer: 4
-    },
-    {
-        question: 'Which country did eggnog come from?',
-        choice1: 'India',
-        choice2: 'England',
-        choice3: 'United States of America',
-        choice4: 'France',
-        answer: 2
     },
     {
         question: 'How do you say “Merry Christmas” in Spanish?',
