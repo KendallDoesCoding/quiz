e10b2f5092d649ad3f10982a9a83bea438f43c6c
Kendall Pinto <kendalldoescoding@gmail.com>
2022-01-08 22:58:29 +0530

    updated

--- tech/game/game.js
+++ tech/game/game.js
@@ -68,7 +68,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/christmas/end')
+        return window.location.assign('/tech/end')
     }
 
     questionCounter++
