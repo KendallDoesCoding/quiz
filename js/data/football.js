d0f9994a64356a698db529ed6a3cb90a2eaed72e
KendallDoesCoding <kendalldoescoding@gmail.com>
2022-12-10 15:21:05 +0530

    feat: major rework and fix breaking changes (#29)

---
+++ js/data/football.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    question: "Who was the best football player of Europe in 1998?",
+    choice1: "Marcel Desailly from Milan and Chelsea",
+    choice2: "Lilian Thuram from Parma",
+    choice3: "Zinedine Zidane from Juventus",
+    choice4: "Michael Owen from Liverpool",
+    answer: 3,
+  },
+  {
+    question: "When was the first Premier League played?",
+    choice1: "1891-92",
+    choice2: "1952-53",
+    choice3: "1985-86",
+    choice4: "1992-93",
+    answer: 4,
+  },
+  {
+    question:
+      "In 1990, this team failed to score even one goal in the World Cup final",
+    choice1: "Argentina",
+    choice2: "Brazil",
+    choice3: "Paris Saint-Germain",
+    choice4: "Portugal",
+    answer: 1,
+  },
+  {
+    question: "Whcih played scores the fastest hat-trick in Premier League",
+    choice1: "Eric Cantona",
+    choice2: "Sadio Mane",
+    choice3: "Brian Deane",
+    choice4: "Rod Wallace",
+    answer: 2,
+  },
+  {
+    question:
+      "Which Barcalona player was jailed after it was found that his password was fake?",
+    choice1: "Jordi Alba",
+    choice2: "Martin Braithwaite",
+    choice3: "Ronald Araujo",
+    choice4: "Ronaldinho",
+    answer: 4,
+  },
+  {
+    question: "Who was the first Premier League manager who was fired?",
+    choice1: "Ian Porterfield",
+    choice2: "George Graham",
+    choice3: "Stewart Houston",
+    choice4: "Bruce Rioch",
+    answer: 1,
+  },
+  {
+    question: "When was the original FA Cup Trophy Stolen? ",
+    choice1: "1900",
+    choice2: "1885",
+    choice3: "1895",
+    choice4: "1845",
+    answer: 3,
+  },
+  {
+    question:
+      "After how many games did Cristiano Ronaldo score his first Champions League goal?",
+    choice1: "27",
+    choice2: "17",
+    choice3: "5",
+    choice4: "36",
+    answer: 1,
+  },
+  {
+    question:
+      "In which club did Nicky Byrne play before finding his calling in Music?",
+    choice1: "Everton",
+    choice2: "Leeds United",
+    choice3: "Tottenham",
+    choice4: "Manchester City",
+    answer: 2,
+  },
+  {
+    question: "Liverpool was founded in which year?",
+    choice1: "1890",
+    choice2: "1950",
+    choice3: "1892",
+    choice4: "1991",
+    answer: 3,
+  },
+];
