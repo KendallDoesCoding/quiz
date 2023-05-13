c553fe0796a09ebb373d6416dee5853c35a70e60
sai140202 <89697419+sai140202@users.noreply.github.com>
2023-05-14 00:59:52 +0530

    feat: display how many answers the users got correct on the end screen.
    * added number of score in last page
    
    * n
    
    * Update pages/end.html
    
    * small updates
    
    * small updates
    
    * updates
    
    ---------
    
    Co-authored-by: Kendall <kendalldoescoding@gmail.com>

--- js/end.js
+++ js/end.js
@@ -1,13 +1,17 @@
 const username = document.querySelector("#username");
 const saveScoreBtn = document.querySelector("#saveScoreBtn");
 const finalScore = document.querySelector("#finalScore");
+const noScore = document.querySelector("#noScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
+const noOfCorrect = localStorage.getItem("noofanswerscorrect");
+const totalQuestions = localStorage.getItem("noofquestions");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
+noScore.innerText = `${noOfCorrect}/${totalQuestions}`;
 
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
@@ -26,8 +30,12 @@ saveHighScore = (e) => {
   highScores.sort((a, b) => {
     return b.score - a.score;
   });
-  highScores.splice(5);
+  highScores.splice(MAX_HIGH_SCORES);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
   window.location.assign("/");
 };
+
+function goBack(n) {
+  history.go(n);
+}
