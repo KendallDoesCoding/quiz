31be39a283922b7d11e4f1b6aeb841a1cbe9b443
Kendall <kendalldoescoding@gmail.com>
2023-05-14 00:56:33 +0530

    small updates

--- js/QuizClass.js
+++ js/QuizClass.js
@@ -4,27 +4,23 @@
 export class Quiz {
   constructor(dataQA = []) {
     this.dataQA = dataQA;
-    // initial game values
     this.QUESTION_VALUE = 100;
     this.QUESTIONS_AMOUNT = dataQA.length;
     this.barPercetage = 0;
     this.score = 0;
     this.answer = "";
     this.canClick = true;
-    this.noOfCorrect=0;
+    this.noOfCorrect = 0;
+    this.TOTAL_CORRECT = 0;
 
-
-    // runs here becasue we want to load the first round of questions
     this._renderNewQuestion();
   }
 
-  // if there's questions left, return the last one
   _newQuestion() {
     if (this.getQuestionsLen === 0) this._endGame();
     return this.dataQA.pop();
   }
 
-  // takes care of managing the percentage green bar on top
   _renderPercentage() {
     const leftQA = this.dataQA.length;
     const percentage = (1 - leftQA / this.QUESTIONS_AMOUNT) * 100;
@@ -32,7 +28,6 @@ export class Quiz {
     document.getElementById("progressBarFull").style.width = `${percentage}%`;
   }
 
-  // need to calculate it based on the total amount, and remaining amount
   _renderQuestionNumber() {
     const leftQA = this.dataQA.length;
     const currentQA = this.QUESTIONS_AMOUNT - leftQA;
@@ -41,7 +36,6 @@ export class Quiz {
     paragraph.textContent = `Question ${currentQA} of ${this.QUESTIONS_AMOUNT}`;
   }
 
-  // get's a new question from newQuestion & renders it to the page
   _renderNewQuestion() {
     const currentQA = this._newQuestion();
     if (!currentQA) {
@@ -57,7 +51,6 @@ export class Quiz {
 
     this.answer = currentQA["answer"];
 
-    // render question && all options
     document.getElementById("question").textContent = currentQA["question"];
 
     pArray.forEach((p, i) => {
@@ -65,41 +58,40 @@ export class Quiz {
     });
   }
 
-  // sends the user to the highscore.html and asks if they want to save their score
   _endGame() {
-    // console.log("GAME ENDED");
     window.localStorage.setItem("mostRecentScore", this.score);
     window.localStorage.setItem("noofanswerscorrect", this.noOfCorrect);
-    
+    window.localStorage.setItem("noofquestions", this.QUESTIONS_AMOUNT);
+  
     window.location.assign("/pages/end.html");
+  }
+  
+  
 
+  _updateCorrectCount() {
+    this.TOTAL_CORRECT++;
+    console.log("Total Correct:", this.TOTAL_CORRECT);
   }
 
   checkAnswer(selected = 0, correct = 0) {
     const p = document.querySelector(`[data-number="${selected}"]`);
 
     if (!this.canClick) {
-      // console.log("WAIT THERE BOI");
       return;
     }
 
     if (selected === correct) {
       p.parentElement.classList.add("correct");
       this.score += this.QUESTION_VALUE;
-      // console.log("NOICE: ", this.score);
-
       this.noOfCorrect++;
-      console.log(this.noOfCorrect);
+      this._updateCorrectCount();
     } else {
       p.parentElement.classList.add("incorrect");
       this.score -= this.QUESTION_VALUE;
-      // console.log("BAKA GA!: ", this.score);
     }
 
-    // render updated score
     document.getElementById("score").textContent = this.score;
 
-    // delay stuff
     this.canClick = false;
 
     setTimeout(() => {
