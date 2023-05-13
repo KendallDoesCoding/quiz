d7a936c80a869b8ca39280a9f755fb8344068c95
KendallDoesCoding <kendalldoescoding@gmail.com>
2023-05-13 13:45:42 +0530

    feat: create specific music list for Christmas songs
    Make a new JS file for only christmas music, for the christmas quiz.
    --- going to add christmas songs soon, as of now there is only 1 SONG.

---
+++ js/christmas-songs.js
@@ -0,0 +1,43 @@
+var songs = [
+    "/music/christmas/AVeryMogulChristmas.mp3",
+  ];
+
+  var audioElement = document.getElementById('my_audio');
+  var musicOnButton = document.getElementById('musicOnButton');
+
+  function playMusic() {
+    var randomIndex = Math.floor(Math.random() * songs.length);
+    var audioSource = document.getElementById('audio_source');
+    audioSource.src = songs[randomIndex];
+    audioElement.load();
+
+    audioElement.addEventListener('canplaythrough', function() {
+      audioElement.play().catch(function(error) {
+        console.log('Error playing audio:', error);
+        pauseMusic();
+      });
+      musicOnButton.style.display = 'none';
+    }, { once: true });
+
+    audioElement.addEventListener('pause', function() {
+      musicOnButton.style.display = 'block';
+    });
+  }
+
+  function pauseMusic() {
+    audioElement.pause();
+    musicOnButton.style.display = 'block';
+  }
+
+  function toggleMusic() {
+    if (audioElement.paused) {
+      playMusic();
+    } else {
+      pauseMusic();
+    }
+  }
+
+  // Play music on page load
+  window.addEventListener('load', function() {
+    playMusic();
+  });
