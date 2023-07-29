ada1bf5b105858e0dfe624ff85fc8c0f336cdb88
Restyled.io <commits@restyled.io>
2023-07-29 14:12:32 +0000

    Restyled by clang-format

--- js/songs.js
+++ js/songs.js
@@ -1,57 +1,55 @@
 var songs = [
-    "/music/apc.mp3",
-    "/music/beachvibes.mp3",
-    "/music/bliss.mp3",
-    "/music/Fluffing-a-Duck.mp3",
-    "/music/happyafricanvillage.mp3",
-    "/music/happyandjoyfulchildren.mp3",
-    "/music/newlands.mp3",
-    "/music/sotb.mp3",
-    "/music/tropicalfever.mp3",
-    "/music/tropicalsoul.mp3",
-    "/music/ukulele.mp3",
-  ];
+  "/music/apc.mp3",
+  "/music/beachvibes.mp3",
+  "/music/bliss.mp3",
+  "/music/Fluffing-a-Duck.mp3",
+  "/music/happyafricanvillage.mp3",
+  "/music/happyandjoyfulchildren.mp3",
+  "/music/newlands.mp3",
+  "/music/sotb.mp3",
+  "/music/tropicalfever.mp3",
+  "/music/tropicalsoul.mp3",
+  "/music/ukulele.mp3",
+];
 
-  var audioElement = document.getElementById('my_audio');
-  var musicOnButton = document.getElementById('musicOnButton');
-  let musicOffButton = document.getElementById('musicOffButton');
+var audioElement = document.getElementById('my_audio');
+var musicOnButton = document.getElementById('musicOnButton');
+let musicOffButton = document.getElementById('musicOffButton');
 
-  function playMusic() {
-    var randomIndex = Math.floor(Math.random() * songs.length);
-    var audioSource = document.getElementById('audio_source');
-    audioSource.src = songs[randomIndex];
-    audioElement.load();
+function playMusic() {
+  var randomIndex = Math.floor(Math.random() * songs.length);
+  var audioSource = document.getElementById('audio_source');
+  audioSource.src = songs[randomIndex];
+  audioElement.load();
 
-    audioElement.addEventListener('canplaythrough', () => {
-      audioElement.play().catch(function(error) {
-        console.log('Error playing audio:', error);
-        pauseMusic();
-      });
-      musicOnButton.style.display = 'none';
-      musicOffButton.style.display = 'block';
-    }, { once: true });
-
-    audioElement.addEventListener('pause', () => {
-      musicOnButton.style.display = 'block';
-      musicOffButton.style.display = 'none';
+  audioElement.addEventListener('canplaythrough', () => {
+    audioElement.play().catch(function(error) {
+      console.log('Error playing audio:', error);
+      pauseMusic();
     });
-  }
+    musicOnButton.style.display = 'none';
+    musicOffButton.style.display = 'block';
+  }, {once : true});
 
-  function pauseMusic() {
-    audioElement.pause();
+  audioElement.addEventListener('pause', () => {
     musicOnButton.style.display = 'block';
     musicOffButton.style.display = 'none';
-  }
-
-  function toggleMusic() {
-    if (audioElement.paused) {
-      playMusic();
-    } else {
-      pauseMusic();
-    }
-  }
-
-  // Play music on page load
-  window.addEventListener('load', function() {
-    playMusic();
   });
+}
+
+function pauseMusic() {
+  audioElement.pause();
+  musicOnButton.style.display = 'block';
+  musicOffButton.style.display = 'none';
+}
+
+function toggleMusic() {
+  if (audioElement.paused) {
+    playMusic();
+  } else {
+    pauseMusic();
+  }
+}
+
+// Play music on page load
+window.addEventListener('load', function() { playMusic(); });
